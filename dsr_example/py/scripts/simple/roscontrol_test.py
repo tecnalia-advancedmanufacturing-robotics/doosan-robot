b9648bcf1f942f69c72ddc38824c59d780a2d9b2
Inigo Moreno <inigo.moreno@tecnalia.com>
2022-04-05 13:10:17 +0200

    Update roscontrol_test.py

--- dsr_example/py/scripts/simple/roscontrol_test.py
+++ dsr_example/py/scripts/simple/roscontrol_test.py
@@ -4,6 +4,10 @@ from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
 from copy import copy
 from math import sin
+import moveit_commander
+import signal, sys
+import numpy as np
+import dsr_msgs.msg
 
 rate=10
 # works with the default example only because I'm not dragging in their libraries to try and parameterize this.
@@ -12,11 +16,10 @@ jscb_count=0
 rob_pos=[]
 rob_vel=[]
 
-def jointstatecallback(msg: JointState):
-    global jscb_count, rob_pos, rob_vel
-    jscb_count=jscb_count+1
-    rob_pos=list(msg.position)
-    rob_pos=list(msg.velocity)
+moveit_commander.roscpp_initialize(sys.argv)
+move_group = moveit_commander.MoveGroupCommander("manipulator")
+
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
 # ROS boilerplate
 print("Setting up node roscontrol_test, I will wiggle the second last joint ~10deg each way.")
@@ -27,41 +30,62 @@ rospy.init_node('roscontrol_test')
 #command = rospy.Publisher("/dsr_control_node/dsr_joint_position_controller/command", Float64MultiArray, queue_size=1)
 #rospy.Subscriber("/dsr_control_node/joint_states", JointState, jointstatecallback)
 
-command = rospy.Publisher("/dsr01h2515/dsr_joint_velocity_controller/command", Float64MultiArray, queue_size=1)
-rospy.Subscriber("/dsr01h2515/joint_states", JointState, jointstatecallback)
+state = None
+def state_cb(state_msg):
+    global state
+    state=state_msg
+
+rospy.Subscriber("/m1509/state", dsr_msgs.msg.RobotState, state_cb)
+
+command = rospy.Publisher("/m1509/dsr_joint_velocity_controller/command", Float64MultiArray, queue_size=1)
 
 governor=rospy.Rate(rate)
-
-# spin waiting for a valid read from the robot, 3 times should be more than enough.
-print("waiting for joint_states")
-while jscb_count<3:
-    governor.sleep()    # this loop wouldn't break without a nap.
-    pass
-print("Got some joint states")
-print(rob_pos)
 
 
 #cmd_pos=Float64MultiArray()
 cmd_vel=Float64MultiArray()
 #cmd_pos.data=rob_pos    # we dont want any joints to move except the second last.
 cmd_vel.data=[0,0,0,0,0,0]   # zero velocity
-init_pos=copy(rob_pos)  # without the copy the most recent value was sneaking in and making things wacky.
 time=0
+
+def sigint_handler(s,f):
+    command.publish(data=[0,0,0,0,0,0])
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
 
 print("Commanding a small wiggle")
 while not rospy.is_shutdown():
     # generate a signal for the robot to track
     #cmd_pos.data[4]=init_pos[4] + (.2*sin(time))
-    cmd_vel.data[4]=.2*sin(time)
+    # cmd_vel.data[4]=.2*sin(time)
+
+
+    amp=0.01
+    vel=[0,0,amp*sin(rospy.Time.now().to_sec()),0,0,0]
+
+    if state is not None:
+        print(state.fWorldETT)
+        vel[2]=state.fWorldETT[2]/500
+        # print("vel3")
+
+    joints=move_group.get_current_joint_values()
+    jacobian=move_group.get_jacobian_matrix(joints)
+    jinv = np.linalg.pinv(jacobian)
+    velj=np.matmul(jinv,np.array(vel))
+    # print("velj="+(velj).__repr__())
+    cmd_vel.data=list(velj)
     #publish command
     #command.publish(cmd_pos)
-    command.publish(cmd_vel)
+    command.publish(data=list(velj))
 
-    print(rob_pos)
     #print(cmd_pos.data)
     print(cmd_vel.data)
     print("")
 
     # 10Hz
     governor.sleep()
-    time=time+(1/rate)
+    time=time+(1./rate)
+
+cmd_vel.data=[0,0,0,0,0,0]   # zero velocity
+command.publish(cmd_vel)
