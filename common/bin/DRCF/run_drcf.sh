62ef8738860ec7a7916f4844924ff7b32684eb83
Andres Montano <andres.montano@tecnalia.com>
2022-05-17 14:48:08 +0200

    FIX: Install issues

--- common/bin/DRCF/run_drcf.sh
+++ common/bin/DRCF/run_drcf.sh
@@ -3,14 +3,14 @@
 echo "Run Emulator of the Doosan Robot Controller (for melodic)"
 
 #echo "Total Param = $#, PROG: $0, param1 =$1, param1 =$2"
-#$1 = server port : 12345 
-#$2 = Robot model      : m0609, m0617, m1013, m1509   
+#$1 = server port : 12345
+#$2 = Robot model      : m0609, m0617, m1013, m1509
 
-echo "dirname:" "$0" 
-echo "server_port:" "$1" 
-echo "robot model:" "$2" 
+echo "dirname:" "$0"
+echo "server_port:" "$1"
+echo "robot model:" "$2"
 
 
-cd "$(dirname "$0")" 
+cd "$(dirname "$0")"
 
-./DRCF64 $1 $2
+sudo ./DRCF64 $1 $2
